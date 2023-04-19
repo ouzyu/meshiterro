c4821ab0c8174db93c8908f47d26cd26abb8e511
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-04-19 09:53:35 +0000

    [Add]投稿,投稿削除,ユーザー編集,機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
