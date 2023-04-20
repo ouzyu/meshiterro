e0f4332f382369e7f9f4dcdca7a2dff898604cec
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-04-20 04:28:49 +0000

    [Start]meshiterro-21,学習開始

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,9 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  varidates :shop_name, presence: true
+  varidates :image, presence: true
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
